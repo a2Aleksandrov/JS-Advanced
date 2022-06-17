631a67e6deaf7681dc17f7fafe4e64c75d705742
Aleksandar <107432011+a2Aleksandrov@users.noreply.github.com>
2022-06-17 20:00:40 +0300

    Delete Syntax Functions and Statements directory

--- Syntax Functions and Statements/EchoFunction.js
+++
@@ -1,7 +0,0 @@
-function EchoFunction(input) {
-    let length = input.length;
-    console.log(length);
-    console.log(input);
-}
-EchoFunction('Hello, JavaScript!');
-EchoFunction('strings are easy');
