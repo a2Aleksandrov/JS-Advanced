1603d50db095f8812e2e483c9a4060e1f7f6044c
Aleksandar <107432011+a2Aleksandrov@users.noreply.github.com>
2022-06-27 14:47:13 +0300

    Add files via upload

--- 10-exam prep/Forum Posts/app.js
+++ 10-exam prep/Forum Posts/app.js
@@ -1,85 +1,90 @@
 window.addEventListener("load", solve);
 
 function solve() {
-  let titleField = document.getElementById('post-title');
-  let categoryField = document.getElementById('post-category');
-  let textField = document.getElementById('post-content');
+  let title = document.getElementById('post-title');
+  let category = document.getElementById('post-category');
+  let content = document.getElementById('post-content');
+  let input = [title, category, content];
+
+  let reviewList = document.getElementById('review-list');
+  let publishedList = document.getElementById('published-list');
+
   let publishBtn = document.getElementById('publish-btn');
-
-  let reviewField = document.getElementById('review-list');
-
   publishBtn.addEventListener('click', publish);
+  function publish(event) {
+    event.preventDefault();
 
-
-  function publish() {
-    if (titleField.value != '' && categoryField.value != '' && textField.value != '') {
-      let li = document.createElement('li');
-      li.className = 'rpost';
-      let article = document.createElement('article');
-      let h4 = document.createElement('h4');
-      let p1 = document.createElement('p');
-      let p2 = document.createElement('p');
-
-      article.appendChild(h4);
-      article.appendChild(p1);
-      article.appendChild(p2);
-
-      li.appendChild(article);
-
-      let editBtn = document.createElement('button');
-      editBtn.textContent = 'Edit'
-      editBtn.className = 'action-btn edit';
-
-      let approveBtn = document.createElement('button');
-      approveBtn.textContent = 'Approve';
-      approveBtn.className = 'action-btn approve';
-
-      li.appendChild(approveBtn);
-      li.appendChild(editBtn);
-
-      reviewField.appendChild(li);
-
-      h4.textContent = titleField.value;
-      let pp1 = categoryField.value;
-      p1.textContent = 'Category: ' + pp1;
-      let pp2 = textField.value;
-      p2.textContent = 'Content: ' + pp2;
-
-      titleField.value = '';
-      categoryField.value = '';
-      textField.value = '';
-
-      let edit = document.getElementsByClassName('action-btn edit')[0];
-      edit.addEventListener('click', editing);
-
-      function editing() {
-
-        titleField.value = document.querySelector('h4').textContent;
-        categoryField.value = pp1;
-        textField.value = pp2;
-
-
-        document.querySelector('.rpost').remove();
-
-      }
-
-      let approve = document.getElementsByClassName('action-btn approve')[0];
-      approve.addEventListener('click', onApprove);
-
-      function onApprove() {
-        let li = document.querySelector('.rpost');
-        let approveField = document.getElementById('published-list');
-        approveField.appendChild(li);
-        editBtn.remove();
-        approveBtn.remove();
-      }
+    if (title.value == '' || category.value == '' || content.value == '') {
+      return;
     }
-  }
-  let clearBtn = document.getElementById('clear-btn');
 
+    //creating review structure
+    let li = document.createElement('li');
+    li.className = 'rpost';
+    let article = document.createElement('article');
+    let h4 = document.createElement('h4');
+    let p1 = document.createElement('p');
+    let p2 = document.createElement('p');
+
+    article.appendChild(h4);
+    article.appendChild(p1);
+    article.appendChild(p2);
+
+    let editBtn = document.createElement('button');
+    editBtn.className = 'action-btn edit';
+    editBtn.textContent = 'Edit';
+    let approveBtn = document.createElement('button');
+    approveBtn.className = 'action-btn approve';
+    approveBtn.textContent = 'Approve';
+
+    li.appendChild(article);
+    li.appendChild(approveBtn);
+    li.appendChild(editBtn);
+
+    reviewList.appendChild(li);
+
+    //adding data from input
+    h4.textContent = title.value;
+    let text1 = category.value;
+    p1.textContent = 'Category: ' + text1;
+    let text2 = content.value;
+    p2.textContent = 'Content: ' + text2;
+    //clearing input
+    title.value = '';
+    category.value = '';
+    content.value = '';
+    //edit btn functionality
+    let edit = li.querySelector('.edit');
+    edit.addEventListener('click', onEdit);
+    function onEdit() {
+      let elements = Array.from(edit.parentElement.children[0].children);
+      console.log(text1, text2)
+      for (let i = 0; i < input.length; i++) {
+        if (i == 1) {
+          input[i].value = text1;
+        } else if (i == 2) {
+          input[i].value = text2;
+        } else {
+          input[i].value = elements[i].textContent;
+        }
+      }
+      edit.parentElement.remove();
+    }
+    //approve btn functionality
+    let approve = li.querySelector('.approve');
+    approve.addEventListener('click', onApprove);
+    function onApprove() {
+      publishedList.appendChild(approve.parentElement);
+      edit.remove();
+      approve.remove();
+    }
+
+  }
+  //clear btn functionality
+  let clearBtn = document.getElementById('clear-btn');
   clearBtn.addEventListener('click', clear);
 
   function clear() {
-    document.querySelector('.rpost').remove();
+    Array.from(clearBtn.parentElement.children[1].children).forEach(child => child.remove());  
   }
 }
