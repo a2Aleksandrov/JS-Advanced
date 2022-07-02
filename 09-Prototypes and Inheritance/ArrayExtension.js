cd29b5af1b324d2ef49ec5200f3bd80318043456
Aleksandar <107432011+a2Aleksandrov@users.noreply.github.com>
2022-07-02 12:12:51 +0300

    Add files via upload

---
+++ 09-Prototypes and Inheritance/ArrayExtension.js
@@ -0,0 +1,26 @@
+(function arrayExtension() {
+    let arr = [1, 2, 3];
+    Array.prototype.last = function () {
+        return this[this.length - 1];
+    }
+    Array.prototype.skip = function (n) {
+        let result = [];
+        for (let i = n; i < this.length; i++) {
+            result.push(this[i]);
+        }
+        return result;
+    }
+    Array.prototype.take = function (n) {
+        let result = [];
+        for (let i = 0; i < n; i++) {
+            result.push(this[i]);
+        }
+        return result;
+    }
+    Array.prototype.sum = function () {
+        return arr.reduce((a, b) => a + b, 0);
+    }
+    Array.prototype.average = function () {
+        return this.reduce((a, b) => a + b) / this.length;
+    }
+})();
